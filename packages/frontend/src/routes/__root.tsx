d5a0e470319f088cf1b75f6281ec5280692d4a1e
ledouxm <mledoux@mledoux.fr>
2025-12-01 13:14:05 +0100

    fix: tab name

--- packages/frontend/src/routes/__root.tsx
+++ packages/frontend/src/routes/__root.tsx
@@ -20,20 +20,32 @@ export const Route = createRootRouteWithContext<Partial<RouterOutputs<"/api/auth
 
 const getTitle = (pathname: string) => {
   if (pathname.startsWith("/edit/")) {
-    return "Compte rendu | CR VIF";
+    return "Patrimoine Embarqué | Compte rendu";
+  }
+
+  if (pathname.startsWith("/constat/")) {
+    return "Patrimoine Embarqué | Constat d'état";
   }
 
   if (pathname.startsWith("/pdf")) {
-    return "PDF | CR VIF";
+    return "Patrimoine Embarqué | PDF";
   }
 
   if (pathname.startsWith("/connection")) {
-    return "Connexion | CR VIF";
+    return "Patrimoine Embarqué | Connexion";
   }
 
   if (pathname.startsWith("/inscription")) {
-    return "Inscription | CR VIF";
+    return "Patrimoine Embarqué | Inscription";
   }
 
-  return "CR VIF";
+  if (pathname.startsWith("/account")) {
+    return "Patrimoine Embarqué | Mon compte";
+  }
+
+  if (pathname.startsWith("/service")) {
+    return "Patrimoine Embarqué | Mon service";
+  }
+
+  return "Patrimoine Embarqué";
 };
