0cf30de7e6afa8aec5ed0ca77f0ff91e3fb34e89
ledouxm <mledoux@mledoux.fr>
2026-01-15 14:33:01 +0100

    feat: add columns

---
+++ db/0010_productive_talon.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "service" ADD COLUMN "courriel_crmh" text;--> statement-breakpoint
+ALTER TABLE "service" ADD COLUMN "courriel_caoa" text;--> statement-breakpoint
+ALTER TABLE "service" ADD COLUMN "courriel_dreal" text;--> statement-breakpoint
+ALTER TABLE "service" ADD COLUMN "courriel_sra" text;--> statement-breakpoint
+ALTER TABLE "service" ADD COLUMN "courriel_udap" text;--> statement-breakpoint
+ALTER TABLE "service" ADD COLUMN "courriel_ofb" text;
