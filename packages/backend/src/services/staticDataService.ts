435a2fd2d1e0dde71ccf3316958487e35af92ad4
ledouxm <mledoux@mledoux.fr>
2026-02-19 14:07:36 +0100

    fix: order services by first word name then by dept_number

--- packages/backend/src/services/staticDataService.ts
+++ packages/backend/src/services/staticDataService.ts
@@ -1,7 +1,14 @@
+import { sql } from "kysely";
 import { db } from "../db/db";
 
 export class StaticDataService {
   async getServices() {
-    return db.selectFrom("service").where("visible", "=", true).orderBy("dept_numbers asc").selectAll().execute();
+    return db
+      .selectFrom("service")
+      .where("visible", "=", true)
+      .orderBy(sql`SPLIT_PART(name, ' ', 1)`)
+      .orderBy("dept_numbers asc")
+      .selectAll()
+      .execute();
   }
 }
