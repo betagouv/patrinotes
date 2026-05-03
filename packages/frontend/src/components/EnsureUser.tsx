25283d37806d3563df96efb0f1d90af97fdb4e1d
ledouxm <mledoux@mledoux.fr>
2026-05-03 14:09:07 +0200

    feat: redirect users from crvif

--- packages/frontend/src/components/EnsureUser.tsx
+++ packages/frontend/src/components/EnsureUser.tsx
@@ -1,13 +1,15 @@
-import { Navigate } from "@tanstack/react-router";
+import { getRouteApi, Navigate } from "@tanstack/react-router";
 import type { PropsWithChildren } from "react";
 import { useUser } from "../contexts/AuthContext";
+const routeApi = getRouteApi("/");
 
 export const EnsureUser = ({ children }: PropsWithChildren) => {
   const user = useUser();
 
+  const params = routeApi.useSearch();
+
   if (!user) {
-    const href = window.location.pathname;
-    return <Navigate to="/connexion" search={{ redirect: href }} />;
+    return <Navigate to={params.from === "crvif" ? "/inscription" : "/connexion"} />;
   }
 
   return <>{children}</>;
