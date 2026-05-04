f76b240b966a787037aa3acda93c4be9ca0a28b0
ledouxm <mledoux@mledoux.fr>
2026-05-04 10:26:01 +0200

    fix: account page crash

--- packages/frontend/src/components/EnsureUser.tsx
+++ packages/frontend/src/components/EnsureUser.tsx
@@ -1,15 +1,12 @@
 import { getRouteApi, Navigate } from "@tanstack/react-router";
 import type { PropsWithChildren } from "react";
 import { useUser } from "../contexts/AuthContext";
-const routeApi = getRouteApi("/");
 
 export const EnsureUser = ({ children }: PropsWithChildren) => {
   const user = useUser();
 
-  const params = routeApi.useSearch();
-
   if (!user) {
-    return <Navigate to={params.from === "crvif" ? "/inscription" : "/connexion"} />;
+    return <Navigate to={"/connexion"} />;
   }
 
   return <>{children}</>;
