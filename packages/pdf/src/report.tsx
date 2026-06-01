85a4a02c8fe887f5ebca4f1a83d061b7ed608b49
ledouxm <mledoux@mledoux.fr>
2026-06-01 09:19:23 +0200

    fix: header texts and bullet list in CR pdf

--- packages/pdf/src/report.tsx
+++ packages/pdf/src/report.tsx
@@ -107,15 +107,15 @@ export const ReportPDFDocument = ({ service, htmlString, images, pictures }: Rep
                 text-transform: uppercase;
               }
 
+              
               .right-texts {
-                text-align: left;
+                text-align: right;
                 margin-top: 13px;
                 display: flex;
-                align-items: flex-start;
+                align-items: flex-end;
                 flex-direction: column;
                 justify-content: flex-start;
-                font-size: 14px;
-                margin-right: 50px;
+                font-size: 12px;
                 max-width: 290px;
               }
 
@@ -128,6 +128,24 @@ export const ReportPDFDocument = ({ service, htmlString, images, pictures }: Rep
               .right-texts > div:nth-child(2) {
               }
 
+
+              li p {
+                display: inline;
+                margin: 0;
+                padding: 0;
+              }
+              ul, ol {
+                padding-left: 20px;
+                margin: 0;
+              }
+
+              li {
+                display: list-item !important;
+                list-style-position: inside;
+              }
+
+              ul li { list-style-type: disc; }
+              ol li { list-style-type: decimal; }
 
 
               .meeting-date {
