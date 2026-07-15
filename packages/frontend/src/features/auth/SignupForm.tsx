378f31548958336eafe770b43cfbf3791fa201f2
ledouxm <mledoux@mledoux.fr>
2026-07-15 11:08:13 +0200

    fix: infotext + add job info on signup page

--- packages/frontend/src/features/auth/SignupForm.tsx
+++ packages/frontend/src/features/auth/SignupForm.tsx
@@ -16,6 +16,7 @@ import { fr } from "@codegouvfr/react-dsfr";
 import { Notice } from "@codegouvfr/react-dsfr/Notice";
 import { JobSelect } from "../../routes/compte";
 import { searchStore } from "#components/SearchModal.tsx";
+import { InfoText } from "#components/ui/InfoText.tsx";
 
 export const SignupForm = () => {
   const form = useForm<SignupFormProps>({
@@ -217,6 +218,10 @@ const SignupJobSelect = ({ form }: { form: UseFormReturn<SignupFormProps> }) => 
   return (
     <Box sx={{ mb: "16px" }}>
       <JobSelect job={job} onChange={(job) => form.setValue("job", job, { shouldDirty: true })} />
+      <InfoText sx={{ fontSize: "12px !important" }}>
+        Votre fonction sera reportée dans les constats et comptes-rendus partagés. Vous pourrez la modifier à tout
+        moment dans votre compte utilisateur.
+      </InfoText>
     </Box>
   );
 };
